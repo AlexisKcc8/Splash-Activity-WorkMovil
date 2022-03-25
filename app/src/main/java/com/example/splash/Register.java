0d551b1711d02f81f466129627fd105606477abe
Alexis Chim <chimalx732@gmail.com>
2022-03-25 16:02:58 -0600

    code_teacher

--- app/src/main/java/com/example/splash/Register.java
+++ app/src/main/java/com/example/splash/Register.java
@@ -2,13 +2,31 @@ package com.example.splash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Register extends AppCompatActivity {
+
+    Button b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        b = findViewById(R.id.btnLogin);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToLogin();
+            }
+        });
+
+    }
+
+    public void goToLogin(){
+        Intent intent = new Intent(Register.this, login.class);
+        startActivity(intent);
     }
 }
