0e89293240bc76cfc1c92af47a6d6305ca7ac484
Alexis Chim <chimalx732@gmail.com>
2022-03-25 12:43:44 -0600

    desing_login

--- app/src/main/java/com/example/splash/login.java
+++ app/src/main/java/com/example/splash/login.java
@@ -2,13 +2,31 @@ package com.example.splash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class login extends AppCompatActivity {
-
+    Button login;
+    TextView registrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        registrar = findViewById(R.id.lbl_registro);
+        registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotToRegister();
+            }
+        });
+    }
+
+    public void gotToRegister(){
+        Intent intent = new Intent(login.this, Register.class);
+        startActivity(intent);
     }
 }
